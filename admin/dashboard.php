9d1327f2dc31245560b40558bc55c5f322d13626
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-19 12:59:49 -0400

    More Admin restructuring and organization; set up styles for Dashboard and "Global" WP admin dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -8,23 +8,32 @@
 */
 
 
-// Get Admin Styles
-add_action( 'admin_enqueue_scripts', 'ucfbands_functionality_styles' );
-function ucfbands_functionality_styles() {
+// Get MKMC Dashboard Styles
+add_action( 'admin_enqueue_scripts', 'mkmc_dashboard_styles' );
+function mkmc_dashboard_styles() {
     
     // Admin Styles
-    wp_enqueue_style( 'admin-styles', plugin_dir_url( __FILE__ ) . 'ucfbands-functionality.css' );
+    wp_enqueue_style( 'admin-styles', plugin_dir_url( __FILE__ ) . 'styles/dashboard.css' );
     
-} // ucfbands_functionality_styles()
+} // mkmc_dashboard_styles()
 
 
 
 // Placeholder
 function mkmc_dashboard() {
+    
     ?>
+
+
+    <!-- WP ADMIN PAGE WRAP -->
     <div class="wrap">
-        <h2>My Plugin Options</h2>
-        your form and page content goes here
-    </div>
+        
+        <h2>MKMC Dashboard</h2>
+        
+        There's gonna be stuff here!
+    
+    </div><!-- /.wrap -->
+
+
     <?php
 }
---
+++ admin/styles/global.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ *  UCFBands Theme Functionality
+ *  Admin: Global Admin Styles
+ *    
+ *  @author Jordan Pakrosnis
+*/
+
+
+// Get MKMC Dashboard Styles
+add_action( 'admin_enqueue_scripts', 'ucfbands_admin_styles' );
+function ucfbands_admin_styles() {
+    
+    // Admin Styles
+    wp_enqueue_style( 'admin-styles', 'global.css' );
+    
+} // ucfbands_admin_styles()

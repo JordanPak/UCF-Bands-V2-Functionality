802914d24afe59299c39b0a5ae2156ce68109306
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-19 12:23:17 -0400

    Add menu registration with Icon and temporary admin dashboard content. Include admin CSS (conditional not working though)

--- admin/admin-menu.php
+++ admin/admin-menu.php
@@ -19,12 +19,28 @@ function add_ucfbands_functionality_menu() {
     //add an item to the menu
     add_menu_page (
         'UCF Bands',
-        'UCF Bands Title',
+        'UCF Bands',
         'manage_options',
         'ucfbands',
         'ucfbands_admin_page',
-        plugin_dir_url( __FILE__ ).'icons/my_icon.png',
+        plugin_dir_url( __FILE__ ).'icons/ucf-icon.png',
         '23.56'
     );
 
 } // add_ucfbands_functionality_menu()
+
+// Register the menu function
+add_action( 'admin_menu', 'add_ucfbands_functionality_menu' );
+
+
+
+
+// Placeholder
+function ucfbands_admin_page() {
+    ?>
+    <div class="wrap">
+        <h2>My Plugin Options</h2>
+        your form and page content goes here
+    </div>
+    <?php
+}

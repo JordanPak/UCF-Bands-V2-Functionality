e95ae8f6fd339e66d0d4068d3089f4c8aca81235
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-19 13:35:32 -0400

    Remove menu icon

--- cpts/announcement.php
+++ cpts/announcement.php
@@ -49,7 +49,6 @@ function ucfbands_cpt_announcement() {
 		'show_in_nav_menus'   => true,
 		'show_in_admin_bar'   => true,
 		'menu_position'       => 1,
-//		'menu_icon'           => 'dashicons-format-quote',
 		'can_export'          => true,
 		'has_archive'         => true,
 		'exclude_from_search' => false,

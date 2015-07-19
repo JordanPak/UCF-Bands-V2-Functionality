71fb1e1718432d7c6845c83b43643f33755255ea
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-19 11:11:38 -0400

    Init plugin file w/ WP comments.

---
+++ ucfbands-functionality.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: UCFBands Theme Functionality
+Plugin URI:  http://ucfbands.com/
+Description: Custom post types, shortcodes, and other functionality for the UCFBands Genesis child theme.
+Version:     2.0
+Author:      Jordan Pakrosnis
+Author URI:  http://JordanPak.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/

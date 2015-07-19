a64c7232a5bee573167639c0e92da6d10d9ee391
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-19 18:30:50 -0400

    Update copied variables, init CMB Fields

--- cpts/rehearsal-schedule.php
+++ cpts/rehearsal-schedule.php
@@ -1,14 +1,14 @@
 <?php
 /*
  *  UCFBands Theme Functionality
- *  CPT: Announcement
+ *  CPT: Rehearsal
  *    
  *  @author Jordan Pakrosnis
 */
 
 
 /**
- * UCFBands CPT: Announcement
+ * UCFBands CPT: Rehearsal
  * Register CPT
  *
  * @author Jordan Pakrosnis
@@ -53,3 +53,68 @@ function ucfbands_cpt_rehearsal() {
 }
 // Hook into the 'init' action
 add_action( 'init', 'ucfbands_cpt_rehearsal', 0 );
+
+
+
+/**
+ * UCFBands CPT: Rehearsal CMB
+ * Register Rehearsal CMB
+ *
+ * @author Jordan Pakrosnis
+ */
+function ucfbands_reahearsal_metabox() {
+	$prefix = '_ucfbands_rehearsal_';
+
+    // Initialize
+    $cmb = new_cmb2_box( array(
+        'id'            => $prefix . 'metabox',
+        'title'         => __( 'Staff Member Details', 'cmb' ),
+        'object_types'  => array( 'ucfbands_staff' ),
+        'context'       => 'normal',
+        'priority'      => 'core',
+    ) );
+    
+    // Is Faculty
+    $cmb->add_field( array(
+        'name' => 'Faculty',
+        'desc' => 'Staff Member is UCF Faculty. Faculty members are listed before non-faculty members in listings.',
+        'id'   => $prefix . 'is_faculty',
+        'type' => 'checkbox'
+    ) );
+    
+    // Position
+    $cmb->add_field( array(
+        'name'    => 'Position',
+        'desc'    => 'Ex: "Director of Bands"',
+        'id'      => $prefix . 'position',
+        'type'    => 'text'
+    ) );
+    
+    // Email Address
+    $cmb->add_field( array(
+        'name' => 'Email Address',
+        'desc' => '@ucf.edu preferred.',
+        'id'   => 'email',
+        'type' => 'text_email',
+    ) );    
+    
+    // Phone Number
+    $cmb->add_field( array(
+        'name'    => 'Phone Number',
+        'desc'    => 'Format: (407) 823-XXXX. UCF Number Preferred',
+        'id'      => $prefix . 'phone',
+        'type'    => 'text'
+    ) );
+    
+    // Biography
+    $cmb->add_field( array(
+    'name'    => 'Biography',
+    'id'      => 'biography',
+    'type'    => 'wysiwyg',
+    'options' => array(
+        'media_buttons' => false,
+    ),
+    ) );
+    
+}
+add_action( 'cmb2_init', 'ucfbands_rehearsal_metabox' );

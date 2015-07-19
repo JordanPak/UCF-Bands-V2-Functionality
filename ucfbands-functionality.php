9602f745b1eb75aa53d3d450a737076d3c6a125a
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-19 11:18:04 -0400

    Outline includes with comments

--- ucfbands-functionality.php
+++ ucfbands-functionality.php
@@ -9,3 +9,66 @@ Author URI:  http://JordanPak.com/
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
+
+
+//=====================//
+//  CUSTOM POST TYPES  //
+//=====================//
+
+// Announcement
+
+
+// Staff Member
+
+
+// Event
+
+
+// Rehearsal Schedule
+
+
+// Photo Gallery (TBA)
+
+
+// Pep Band (TBA)
+
+
+
+//==============//
+//  SHORTCODES  //
+//==============//
+
+
+//-- GENERAL --//
+
+// Button
+
+
+// Content Box
+
+
+// Block
+
+
+// Hero (Bootstrap Jumbotron-like thing)
+
+
+
+//-- CPT-BASED --//
+
+// Announcement & Announcements Listing
+
+
+// Staff Member Listing
+
+
+// Event & Events Listing
+
+
+// Rehearsal Listings
+
+
+// Photo Gallery & Photo Galleries (TBA)
+
+
+// Pep Band & Pep Band Listing (TBA)

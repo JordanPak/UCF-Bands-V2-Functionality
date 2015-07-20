c72f1d477399f50607dec07a9994dfdc1a6c5d8b
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-20 11:23:11 -0400

    Add default attributes & variables for them

--- shortcodes/button.php
+++ shortcodes/button.php
@@ -14,11 +14,34 @@
  */
 function ucfbands_shortcode_button( $atts ) {
     
+    //-- ATTRIBUTES --//
 	$atts = shortcode_atts( array(
-		'foo' => 'no foo',
-		'baz' => 'default baz'
-	), $atts, 'bartag' );
+        'text'      => '',
+        'size'      => 'med',
+        'color'     => '',
+        'url'       => '#',
+        'target'    => '',
+        'icon'      => '',
+	), $atts, 'button' );
 
+    
+    //-- SET VARS --//
+    
+    // Attributes
+    $button_text =      $atts['text'];
+    $button_size =      $atts['size'];
+    $button_color =     $atts['color'];
+    $button_url =       $atts['url'];
+    $button_target =    $atts['target'];
+    $button_icon =      $atts['icon'];
+    
+    // Output
+    $shortode_output = '';
+    
+    
+    
+    
+    
 	return "foo = {$atts['foo']}";
     
 } // ucfbands_shortcode_button()

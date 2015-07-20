14c75de4f998014c4264c975af0f6f32a9e5171d
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-20 11:33:49 -0400

    Edit comments & re-order attributes to be used in order in the rest of the code

--- shortcodes/button.php
+++ shortcodes/button.php
@@ -14,43 +14,59 @@
  */
 function ucfbands_shortcode_button( $atts ) {
     
+    
     //-- ATTRIBUTES --//
 	$atts = shortcode_atts( array(
-        'text'      => '',
         'size'      => 'med',
         'color'     => '',
         'outline'   => 'no',
         'url'       => '#',
         'target'    => '',
         'icon'      => '',
+        'text'      => '',
 	), $atts, 'button' );
 
+    
     
     //-- SET VARS --//
     
     // Attributes
-    $button_text =      $atts['text'];
     $button_size =      $atts['size'];
     $button_color =     $atts['color'];
     $button_outline =   $atts['outline'];
     $button_url =       $atts['url'];
     $button_target =    $atts['target'];
     $button_icon =      $atts['icon'];
+    $button_text =      $atts['text'];
     
     // Output
     $shortode_output = '';
     
     
-    //-- LOGIC --//
+    
+    //=========//
+    //  LOGIC  //
+    //=========//
+    
+    
+    //-- CLASSES --//
+    
+    // 
     
     
     
     
-    //-- OUTPUT --//
+    //==========//
+    //  OUTPUT  //
+    //==========//
+    
+    // Open Tag
+    $shortcode_output .= '<a ';
     
     
-    $shortcode_output = '<a class="button">Look at this button</a>';
     
+    // Close Tag
+    $shortcode_output .= '</a>';
     
     
     // Return Output String

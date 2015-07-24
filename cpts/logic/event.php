a5590bdaccf0a051444259cd3fe30ade7682bb27
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-24 15:12:06 -0400

    Finish primary query logic

--- cpts/logic/event.php
+++ cpts/logic/event.php
@@ -11,11 +11,36 @@
  * UCFBands Event: Build Query
  *
  * @author Jordan Pakrosnis
+ * @return Event Query 
  */
 function ucfbands_event_query( $num_events, $band ) {
     
     
+    // Taxonomy Query //
+    $tax_query = array(
+        array(
+            'taxonomy'  => 'band',
+            'field'     => 'slug',
+            'terms'     => $band,
+        ),
+    );
     
+    // Event Query Args
+    $events_args = array(
+        'post_type'         => 'event',
+        'tax_query'         => $tax_query,
+        'fields'            => 'ids',
+        'orderby'           => 'meta_value_num',
+        'order'             => 'DSC',
+        'post_count'        => $events_num,
+        'posts_per_page'    => $events_num,
+    );
     
+    // Query/Get Post IDs
+    $events = new WP_Query( $events_args );
     
 } // ucfbands_event_query()
+
+
+
+

d1cd2fb43e4cbebf175ed4fec5911c7175febba8
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-24 15:03:50 -0400

    Create event query fn

--- cpts/logic/event.php
+++ cpts/logic/event.php
@@ -5,3 +5,17 @@
  *    
  *  @author Jordan Pakrosnis
 */
+
+
+/**
+ * UCFBands Event: Build Query
+ *
+ * @author Jordan Pakrosnis
+ */
+function ucfbands_event_query( $num_events, $band ) {
+    
+    
+    
+    
+    
+} // ucfbands_event_query()

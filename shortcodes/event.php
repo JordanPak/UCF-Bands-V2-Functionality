71844534104ba13057aad9646af1a7fbe99b81e3
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-24 15:30:24 -0400

    Configure shortcode "none found" message

--- shortcodes/event.php
+++ shortcodes/event.php
@@ -65,6 +65,36 @@ function ucfbands_shortcode_events( $atts ) {
     $shortcode_output .= $events_wrap_open;
     
     
+    //-- NONE FOUND MESSAGE --//
+    
+    // If no matches, set the message
+    if ( ($events->have_posts()) == false ) {
+        
+        // Get Band Name
+        $events_band_name = get_term_by( 'slug', $events_band, 'band')->name;
+        
+        // If "all-bands", just do "There are currently no announcements"
+        if ( strtolower($events_band_name) != 'all bands') {
+        
+            // None Found Message
+            $shortcode_output .=
+                '<p>There are currently no events for the ' . $events_band_name . '.</p>' . $events_wrap_close;
+            
+            
+        } // If not "All Bands"
+        
+        // If "All Bands"
+        else {
+            $shortcode_output .= '<p>There are currently no events.</p>' . $events_wrap_close;
+        }
+        
+        // Finish Shortcode FN with output.
+        return $shortcode_output;
+    
+    } // If None Found        
+    
+    
+    
     // Wrap Close
     $shortcode_output .= $events_wrap_close;
     

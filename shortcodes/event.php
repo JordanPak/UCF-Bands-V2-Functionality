81a27604adfa8e40886177b5d837312e13b8fc66
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-24 15:19:13 -0400

    Configure events shortcode attributes and vars

--- shortcodes/event.php
+++ shortcodes/event.php
@@ -13,11 +13,38 @@
  * @author Jordan Pakrosnis
  */
 function ucfbands_shortcode_events( $atts ) {
+
+    
+    //-- ATTRIBUTES --//
+	$atts = shortcode_atts( array(
+        'num'       => '3',         // Number of announcements to show
+        'heading'   => '',          // Show "Events" Heading
+        'button'    => '',          // Show "View All" Button next to heading
+        'band'      => 'all-bands', // Slug for band in Band taxonomy
+	), $atts, 'announcements' );
+    
+    
+    //-- SET VARS --//
+    
+    // Attributes
+    $events_num =        $atts['num'];
+    $events_heading =    $atts['heading'];
+    $events_button =     $atts['button'];
+    $events_band =       $atts['band'];
+    
+    // Helpers
+    $events =           ''; // WP_Query()
+    $no_events_found =  '';
+    
+    // Output
+    $shortode_output =  '';    
+    
     
     
     //=========//
     //  LOGIC  //
     //=========//
+    
     
     // Get Events
     $events = ucfbands_event_query( 5, 'all-bands' );

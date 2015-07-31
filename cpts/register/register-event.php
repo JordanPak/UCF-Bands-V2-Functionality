9f249e68be9d01237103a3a2b8e1e3806d0c3caf
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-31 19:47:01 -0400

    Change sub-item to sub_item; reformat schedule time output

--- cpts/register/register-event.php
+++ cpts/register/register-event.php
@@ -183,6 +183,7 @@ function ucfbands_event_metabox() {
         'name' => 'Time',
         'id'   => 'time',
         'type' => 'text_time',
+        'time_format' => 'g:i a',
     ) );
     
     // Schedule Group: Listing Thing
@@ -195,7 +196,7 @@ function ucfbands_event_metabox() {
     // Schedule Group: Sub-Item
     $cmb->add_group_field( $group_field_id, array(
         'name' => 'Sub-Items (Optional)',
-        'id'   => 'sub-item',
+        'id'   => 'sub_item',
         'type' => 'text',
         'repeatable' => true,
     ) );    

fc01fd73affab653c6e0181fc79bad7dfe2d7190
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-01 15:23:06 -0400

    Comment-out existing require_once fn calls; Move Announcements

--- ucfbands-functionality.php
+++ ucfbands-functionality.php
@@ -33,75 +33,83 @@ require_once( 'admin/styles/global.php' );
 //require_once( 'admin/dashboard.php' );
 
 
-//====================//
-//  CPT REGISTRATION  //
-//====================//
+////====================//
+////  CPT REGISTRATION  //
+////====================//
+//
+//// Announcement
+//require_once( 'cpts/register/register-announcement.php' );
+//
+//// Staff Member
+//require_once( 'cpts/register/register-staff-member.php' );
+//
+//// Event
+//require_once( 'cpts/register/register-event.php' );
+//
+//// Rehearsal Schedule
+//require_once( 'cpts/register/register-rehearsal-schedule.php' );
+//
+//// Photo Gallery (TBA)
+////require_once( 'cpts/register/register-photo-gallery.php' );
+//
+//// Pep Band (TBA)
+////require_once( 'cpts/register/register-pep-band.php' );
+//
+//
+//
+////=============//
+////  CPT LOGIC  //
+////=============//
+//
+//// Events
+//require_once( 'cpts/logic/logic-event.php' );
+//
+//
+//
+////==============//
+////  SHORTCODES  //
+////==============//
+//
+//
+////-- GENERAL --//
+//
+//// Button
+//require_once( 'shortcodes/shortcode-button.php' );
+//
+//// Content Box
+//require_once( 'shortcodes/shortcode-content-box.php' );
+//
+//// Hero (Bootstrap Jumbotron-like thing)
+//require_once( 'shortcodes/shortcode-hero.php' );
+//
+//// Block (TBA)
+////require_once( 'shortcodes/shortcode-block.php' );
+//
+//
+////-- CPT-BASED --//
+//
+//// Announcement & Announcements Listing
+//require_once( 'shortcodes/shortcode-announcement.php' );
+//
+//// Staff Member Listing
+////require_once( 'shortcodes/shortcode-staff-member.php' );
+//
+//// Event & Events Listing
+//require_once( 'shortcodes/shortcode-event.php' );
+//
+//// Rehearsal Listings
+////require_once( 'shortcodes/shortcode-rehearsal.php' );
+//
+//// Photo Gallery & Photo Galleries (TBA)
+////require_once( 'shortcodes/shortcode-photo-gallery.php' );
+//
+//// Pep Band & Pep Band Listing (TBA)
+////require_once( 'shortcodes/shortcode-pep-band.php' );
 
-// Announcement
-require_once( 'cpts/register/register-announcement.php' );
 
-// Staff Member
-require_once( 'cpts/register/register-staff-member.php' );
+//================//
+//  ANNOUNCEMENT  //
+//================//
 
-// Event
-require_once( 'cpts/register/register-event.php' );
-
-// Rehearsal Schedule
-require_once( 'cpts/register/register-rehearsal-schedule.php' );
-
-// Photo Gallery (TBA)
-//require_once( 'cpts/register/register-photo-gallery.php' );
-
-// Pep Band (TBA)
-//require_once( 'cpts/register/register-pep-band.php' );
-
-
-
-//=============//
-//  CPT LOGIC  //
-//=============//
-
-// Events
-require_once( 'cpts/logic/logic-event.php' );
-
-
-
-//==============//
-//  SHORTCODES  //
-//==============//
-
-
-//-- GENERAL --//
-
-// Button
-require_once( 'shortcodes/shortcode-button.php' );
-
-// Content Box
-require_once( 'shortcodes/shortcode-content-box.php' );
-
-// Hero (Bootstrap Jumbotron-like thing)
-require_once( 'shortcodes/shortcode-hero.php' );
-
-// Block (TBA)
-//require_once( 'shortcodes/shortcode-block.php' );
-
-
-//-- CPT-BASED --//
-
-// Announcement & Announcements Listing
-require_once( 'shortcodes/shortcode-announcement.php' );
-
-// Staff Member Listing
-//require_once( 'shortcodes/shortcode-staff-member.php' );
-
-// Event & Events Listing
-require_once( 'shortcodes/shortcode-event.php' );
-
-// Rehearsal Listings
-//require_once( 'shortcodes/shortcode-rehearsal.php' );
-
-// Photo Gallery & Photo Galleries (TBA)
-//require_once( 'shortcodes/shortcode-photo-gallery.php' );
-
-// Pep Band & Pep Band Listing (TBA)
-//require_once( 'shortcodes/shortcode-pep-band.php' );
+require_once( 'announcement/announcement-cpt.php' );
+require_once( 'announcement/announcement-shortcode.php' );

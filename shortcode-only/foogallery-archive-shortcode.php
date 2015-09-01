acc407847187716b7cb7e9f3e364cac9f20c1468
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-09-01 16:54:14 -0400

    Remove vars, output to div  with ul wrapper.

--- shortcode-only/foogallery-archive-shortcode.php
+++ shortcode-only/foogallery-archive-shortcode.php
@@ -5,3 +5,66 @@
  *
  *  @author Jordan Pakrosnis
 */
+
+
+/**
+ * UCFBands Shortcode: FooGallery Archive
+ *
+ * @author Jordan Pakrosnis
+ */
+function ucfbands_shortcode_foogallery_archive( $atts, $content = "" ) {
+
+
+    //-- ATTRIBUTES --//
+  	$atts = shortcode_atts( array(
+  	), $atts, 'foogallery-archive' );
+
+
+    //-- SET VARS --//
+
+    // Output
+    $shortode_output = '';
+
+
+
+    //=========//
+    //  LOGIC  //
+    //=========//
+
+    //-- CLASSES --//
+
+
+
+
+    //==========//
+    //  OUTPUT  //
+    //==========//
+
+    // Start Wrap
+    $shortcode_output .= '<div class="foogallery-archive">';
+
+
+        // Archive Title
+        $shortcode_output .= '<span class="foogallery-archive-title">Archive</span>';
+
+
+        // list
+        $shortcode_output .= '<ul>';
+
+            // Content (Link Shortcodes)
+            $shortcode_output .= do_shortcode($content);
+
+        $shortcode_output .= '</ul>';
+
+
+    // Closing Tag
+    $shortcode_output .= '</div>';
+
+
+    // Return Output String
+	return $shortcode_output;
+
+} // ucfbands_shortcode_button()
+
+// Register the shortcode
+add_shortcode( 'foogallery-archive', 'ucfbands_shortcode_foogallery_archive' );

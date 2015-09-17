4bf2966eb2cd7e93e465e212117affc80cac2e3e
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-09-17 13:36:29 -0400

    Add note about schedule

--- schedule/schedule-cmb2.php
+++ schedule/schedule-cmb2.php
@@ -21,8 +21,8 @@ function ucfbands_schedule_shortcode_metabox() {
    	// Get the data
    	$id = $post->ID;
 
-   	// Echo out the field
    	echo '<code style="display: inline-block; margin-top: 5px; padding: 12px 20px;">[schedule id="' . $id . '"]</code>';
+	echo '<p>Use this shortcode in a box to display the schedule.</p>';
 
 } // ucfbands_schedule_shortcode_metabox()
 

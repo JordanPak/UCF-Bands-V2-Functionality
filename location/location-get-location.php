2071882373e59ebbadbec41697d7ff3a6d6fc357
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-10-03 19:54:16 -0400

    Add Get Google Map Fn, Fix Get Address Fn

--- location/location-get-location.php
+++ location/location-get-location.php
@@ -7,6 +7,7 @@
  *  @author Jordan Pakrosnis
 */
 
+
 /**
  * UCFBands CPT: Location
  * Get Location Name
@@ -17,6 +18,8 @@ function ucfbands_location_get_name ( $location ) {
     return get_the_title( $location );
 }
 
+
+
 /**
  * UCFBands CPT: Location
  * Get Location Address
@@ -25,11 +28,16 @@ function ucfbands_location_get_name ( $location ) {
  */
 function ucfbands_location_get_address( $location ) {
 
-    $location_meta = ucfbands_location_get_meta( $location );
+    if ( $location != '' ) {
+        $location_meta = ucfbands_location_get_meta( $location );
+        $address = $location_meta['address'];
 
-    $address = $location_meta['address'];
+        return '<b>' . get_the_title( $location ) . '</b>' . ucfbands_location_address( $address );
+    }
 
-    return ucfbands_location_address( $address );
+    else {
+        return 'To Be Announced';
+    }
 
 } // ucfbands_get_location_address()
 
@@ -42,5 +50,22 @@ function ucfbands_location_get_address( $location ) {
  * @author Jordan Pakrosnis
  */
 function ucfbands_location_get_google_map( $location ) {
+
+    if ( $location != '' ) {
+
+        $location_meta = ucfbands_location_get_meta( $location );
+
+        $latitude = $location_meta['google_map_latitude'];
+        $longitude = $location_meta['google_map_longitude'];
+
+
+        // if google map, continue. Else, output TBA.
+        if ( ($latitude != '') && ($longitude != '') ) {
+            ucfbands_location_google_map( $latitude, $longitude );
+        }
+
+    } // if location exists
+
+    return;
 
 } // ucfbands_get_location_google_map()
